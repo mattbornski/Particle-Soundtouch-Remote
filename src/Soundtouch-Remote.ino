250911912a5e465de4ecf93011ea03007429318b
Matt Bornski <matt@bornski.com>
2017-11-15 07:10:06 -0800

    controlling LED ring directly via rotary encoder

--- src/Soundtouch-Remote.ino
+++ src/Soundtouch-Remote.ino
@@ -7,20 +7,60 @@
 
 #include "Particle.h"
 #include "neopixel.h"
+#include <Encoder.h>
 
 #define PIXEL_PIN A5
 #define PIXEL_COUNT 24
 #define PIXEL_TYPE WS2812B
 
+#define BUTTON_PIN A4
+
+
 Adafruit_NeoPixel ring(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+Encoder volumeKnob(D3, D4);
+static volatile uint8_t volumePercentage = 0;
+static volatile bool recentlyPressed = false;
 
 void setup() {
     ring.begin();
+    // This thing is bright.  255 = max, 0 = off
+    ring.setBrightness(16);
     ring.show();
+
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    attachInterrupt(BUTTON_PIN, press, FALLING);
 }
 
 void loop() {
-    rainbow(20);
+    if (recentlyPressed) {
+        Serial.println("Button recently pressed");
+        recentlyPressed = false;
+    }
+
+    setArc(volumePercentage, 0xFFFFFFFF);
+
+    static volatile long oldPosition = -999;
+    long newPosition = volumeKnob.read();
+    if (newPosition != oldPosition) {
+        oldPosition = newPosition;
+        Serial.println(newPosition);
+        volumePercentage = newPosition;
+    }
+}
+
+void setArc(uint8_t percentage, uint32_t color) {
+    uint8_t illuminatedPixels = (uint8_t) (ring.numPixels() * ((float)percentage / 100.0f));
+    Serial.print("illuminate ");
+    Serial.print(illuminatedPixels);
+    Serial.println(" / 24 pixels");
+    for (uint8_t i = 0; i < ring.numPixels(); i++) {
+        if (i < illuminatedPixels) {
+            ring.setPixelColor(i, color);
+        } else {
+            ring.setPixelColor(i, (uint32_t)0);
+        }
+    }
+    ring.show();
 }
 
 void rainbow(uint8_t wait) {
@@ -47,4 +87,14 @@ uint32_t Wheel(byte WheelPos) {
    WheelPos -= 170;
    return ring.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
+}
+
+void press() {
+    static volatile unsigned long lastInterruptTime = 0;
+    unsigned long interrupt_time = millis();
+    // debounce time = 50milliseconds
+    if (interrupt_time - lastInterruptTime > 50) {
+        recentlyPressed = true;
+    }
+    lastInterruptTime = interrupt_time;
 }
